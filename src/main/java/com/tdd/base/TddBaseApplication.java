e095339e88577b7177a9639bdc9fd9ec77945b5c
Adriano T. Tanaka <adritanaka@gmail.com>
2016-03-09 00:59:43 -0300

    Criação projeto no eclipse
    Commit com projeto base do eclipse

---
+++ src/main/java/com/tdd/base/TddBaseApplication.java
@@ -0,0 +1,12 @@
+package com.tdd.base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TddBaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TddBaseApplication.class, args);
+	}
+}
